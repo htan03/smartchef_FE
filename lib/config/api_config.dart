842472dca41bdbeb80b47a7f7bb03b9016e897f5
thaitrongnguyennn29 <thaitrongnguyennn29@gmail.com>
2025-12-23 01:06:48 +0700

    Sửa giờ trong lịch sử nấu ăn (đúng giờ Việt Nam); Sửa chữ ở nhập nguyên liệu để gợi ý cho đẹp

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 class ApiConfig {
   
-  // static const String baseUrl = "http://192.168.10.236:8000";
-  static String baseUrl = Platform.isAndroid 
-      ? "http://10.0.2.2:8000" 
-      : "http://127.0.0.1:8000";
+  static const String baseUrl = "http://192.168.1.122:8000";
+  // static String baseUrl = Platform.isAndroid 
+  //     ? "http://10.0.2.2:8000" 
+  //     : "http://127.0.0.1:8000";
       
 }
